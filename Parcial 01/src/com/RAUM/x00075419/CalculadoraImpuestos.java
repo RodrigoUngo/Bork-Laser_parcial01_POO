ba6def61c2794cbffc904e5d2b6efd766b63b6ae
RodrigoUngo <00075419@uca.edu.sv>
2020-04-18 21:48:11 -0600

    Add: CalculadoraImpuestos.java

---
+++ Parcial 01/src/com/RAUM/x00075419/CalculadoraImpuestos.java
@@ -0,0 +1,41 @@
+package com.RAUM.x00075419;
+
+public final class CalculadoraImpuestos {
+    private double totalRenta, totalISSS, totalAFP;
+
+    private CalculadoraImpuestos() {
+    }
+
+    public double calcularPago(Empleado p){
+        double Renta = 0, ISSS = 0, AFP = 0, restante = 0;
+        if(p instanceof ServicioProfesional){
+            Renta = 0.1*p.salario;
+            totalRenta += Renta;
+            return p.salario - Renta;
+        }
+        else{
+            AFP = 0.0625*p.salario;
+            ISSS = 0.03*p.salario;
+            restante = p.salario - AFP - ISSS;
+            if(restante >= 0.01 && restante <= 472)
+                Renta = 0;
+            else if(restante >= 472.01 && restante <= 895.24)
+                Renta = 0.1*(restante - 472) + 17.67;
+            else if(restante >= 895.25 && restante <= 2038.10)
+                Renta = 0.2*(restante - 895.24) + 60;
+            else if(restante >= 2038.11)
+                Renta = 0.3*(restante - 2038.10) + 288.57;
+            totalRenta += Renta;
+            totalISSS += ISSS;
+            totalAFP += AFP;
+            return restante - Renta;
+        }
+    }
+
+    public String mostrarTotales() {
+        return "Total Renta: " + totalRenta +
+                ", Total SSS: " + totalISSS +
+                ", Total AFP: " + totalAFP +
+                '}';
+    }
+}
